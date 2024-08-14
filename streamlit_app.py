a5570f3567881cd065dc44d64dbb4adf574001bd
world-source <jcmye1@schools.vic.edu.au>
2024-08-14 10:36:21 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,14 +4,12 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-label = "Enter text here"
-st.text_input(Hello)
-
+label = Hello
 st.components.v1.html(
     f"""
     <script>
         var elems = window.parent.document.querySelectorAll('div[class*="stTextInput"] p');
-        var elem = Array.from(elems).find(x => x.innerText == '{Hello}');
+        var elem = Array.from(elems).find(x => x.innerText == 'label');
         elem.style.fontSize = '20px'; // the fontsize you want to set it to
     </script>
     """
