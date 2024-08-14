631475018d74a6bcd45627d033cb5c655d3674ad
world-source <jcmye1@schools.vic.edu.au>
2024-08-14 10:34:07 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+s = f"<p style='font-size:20px;'>{Hi}</p>"
+st.markdown(s, unsafe_allow_html=True) 
