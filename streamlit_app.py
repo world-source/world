be51d25c6117feb8af7591c4a82e9e268c96ae8b
world-source <jcmye1@schools.vic.edu.au>
2024-08-14 10:35:55 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,15 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-s = f"<p style='font-size:20px;'>{Hi}</p>"
-st.markdown(s, unsafe_allow_html=True) 
+label = "Enter text here"
+st.text_input(Hello)
+
+st.components.v1.html(
+    f"""
+    <script>
+        var elems = window.parent.document.querySelectorAll('div[class*="stTextInput"] p');
+        var elem = Array.from(elems).find(x => x.innerText == '{Hello}');
+        elem.style.fontSize = '20px'; // the fontsize you want to set it to
+    </script>
+    """
+)
